5e728275785a66117b7b5fe3c5d83ed3bcae3022
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-18 09:49:08 -0800

    ContentView will house the Tabview. TransactionsListView will present the list of transactions and allow you to add new transactions. MerchantListView will show you the list of merchants, allow adding tags to merchants for easier grouping, and allow new merchants to be added

---
+++ Money Saver/MerchantListView.swift
@@ -0,0 +1,20 @@
+//
+//  MerchantListView.swift
+//  Money Saver
+//
+//  Created by Rhys Julian-Jones on 1/18/24.
+//
+
+import UIKit
+
+class MerchantListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Money Saver/TransactionListView.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionListView.swift
+//  Money Saver
+//
+//  Created by Rhys Julian-Jones on 1/18/24.
+//
+
+import UIKit
+
+class TransactionListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
