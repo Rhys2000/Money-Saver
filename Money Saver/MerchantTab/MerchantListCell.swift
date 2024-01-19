dbfd4512081757904451584d299dc5a6f9c5f1b1
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-19 09:08:07 -0800

    Added the basics of the MerchantListCell

--- Money Saver/MerchantTab/MerchantListCell.swift
+++ Money Saver/MerchantTab/MerchantListCell.swift
@@ -12,7 +12,23 @@ struct MerchantListCell: View {
     let currentMerchant: Merchant
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text(currentMerchant.name)
+                .padding(.leading, 10)
+                .foregroundColor(.white)
+                .bold()
+            Spacer()
+            Text(currentMerchant.tag)
+                .padding(.trailing, 10)
+                .foregroundColor(.white)
+                .bold()
+        }
+        .frame(maxWidth: .infinity)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.blue) // Change the color as per your design
+                .frame(height: 50)
+        )
     }
 }
 
