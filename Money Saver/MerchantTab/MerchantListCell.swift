4f0599cfca681fbf8e201b0405d6843c17d02005
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-19 09:03:53 -0800

    Background Info

---
+++ Money Saver/MerchantTab/MerchantListCell.swift
@@ -0,0 +1,21 @@
+//
+//  MerchantListCell.swift
+//  Money Saver
+//
+//  Created by Rhys Julian-Jones on 1/19/24.
+//
+
+import SwiftUI
+
+struct MerchantListCell: View {
+    
+    let currentMerchant: Merchant
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MerchantListCell()
+}
