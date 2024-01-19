7344e26bab354db3fcc2c6d47eda07910b3a05dd
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-19 08:06:06 -0800

    Background changes

---
+++ Money Saver/Components/AddButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddButton.swift
+//  Money Saver
+//
+//  Created by Rhys Julian-Jones on 1/19/24.
+//
+
+import SwiftUI
+
+struct AddButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddButton()
+}
