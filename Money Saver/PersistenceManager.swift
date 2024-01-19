da5d1eb1b1942835ca8055c796c0816f7af322b2
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-19 08:35:55 -0800

    Removed the placeholder code and function deleteMerchant, changed saveMerchants to be a public function so it can be used on the MerchantListView

--- Money Saver/PersistenceManager.swift
+++ Money Saver/PersistenceManager.swift
@@ -28,16 +28,7 @@ class PersistenceManager {
         return []
     }
     
-    func deleteMerchant() {
-//        var savedMerchants = loadMerchants()
-//        
-//        if let indexToDelete = savedMerchants.firstIndex(where: { $0.id == merchantIDToDelete }) {
-//            savedMerchants.remove(at: indexToDelete)
-//            saveMerchants(merchants: savedMerchants)
-//        }
-    }
-    
-    private func saveMerchants(merchants: [Merchant]) {
+    func saveMerchants(merchants: [Merchant]) {
         if let data = try? JSONEncoder().encode(merchants) {
             UserDefaults.standard.set(data, forKey: merchantEntriesKey)
         }
