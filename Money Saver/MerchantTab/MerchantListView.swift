caa4e9e6245a1ef39c01da82fa4463ad2b38f9cd
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-22 11:33:36 -0800

    Completely refactoring how the list is displayed causing numerous downstream changes. Complete rewrite of this part of the application

--- Money Saver/MerchantTab/MerchantListView.swift
+++ Money Saver/MerchantTab/MerchantListView.swift
@@ -6,72 +6,34 @@ import SwiftUI
 
 struct MerchantListView: View {
     
-    @State private var addingNewMerchant = false
-    @State private var merchants: [Merchant] = []
-    @State private var isEditing = false
+    //@State private var merchants: [Merchant] = PersistenceManager.shared.loadMerchants()
+    @State private var merchants = [Merchant(name: "Rhys", tag: .groceries), Merchant(name: "Trader", tag: .homegoods)]
     
     var body: some View {
         NavigationView {
             VStack {
-                
-                // List of Merchants
                 List {
-                    //Turns each Merchant in the saved data into a row in the List
-                    ForEach(merchants, id: \.name) { merchant in
-                        MerchantListCell(currentMerchant: merchant)
-                            .listRowInsets(EdgeInsets())
-                            .listRowBackground(Color.clear)
-                            .padding(.vertical)
-                            //.listRowSeparator(.hidden)
-                            .background(Color.yellow)
+                    ForEach(merchants.indices, id: \.self) { index in
+                        Button(action: {
+                            withAnimation {
+                                self.merchants[index].isExpanded.toggle()
+                            }
+                        }) {
+                            Text(self.merchants[index].name)
+                        }
                     }
                     .onDelete(perform: deleteMerchant)
                 }
-                .onAppear {
-                    merchants = PersistenceManager.shared.loadMerchants()
-                }
-                .id(addingNewMerchant)
-                .sheet(isPresented: $addingNewMerchant) {
-                    NewMerchantView(isDisplayed: $addingNewMerchant)
-                }
-                
-                Spacer()
             }
-            .navigationBarTitle("Merchant List")
             .navigationBarItems(leading: EditButton())
-            .navigationBarItems(trailing: AddButton(openSheet: $addingNewMerchant))
         }
     }
     
-    func deleteMerchant(at offsets: IndexSet) {
+    private func deleteMerchant(at offsets: IndexSet) {
         merchants.remove(atOffsets: offsets)
-        PersistenceManager.shared.saveMerchants(merchants: merchants)
     }
 }
 
-struct Merchant: Identifiable, Codable {
-    var id = UUID()
-    var name: String
-    var tag: Tag
-}
-
-enum Tag: String, Codable, CaseIterable {
-    case none = "None"
-    case groceries = "Groceries"
-    case homegoods = "Homegoods"
-    
-    var isEmpty: Bool {
-        self == .none ? true : false
-    }
-    
-    var color: Color {
-        switch self {
-        case .none:
-            return Color.gray
-        case .groceries:
-            return Color.green
-        case .homegoods:
-            return Color.red
-        }
-    }
+#Preview {
+    MerchantListView()
 }
