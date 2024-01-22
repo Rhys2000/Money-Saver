32347cb5b6b95920b0f591e7851e5fa9efd7bec2
Rhys Julian-Jones <rhys2kjj@gmail.com>
2024-01-22 11:32:07 -0800

    Moved the Merchant struct and Tag enum to Merchant.swift

--- Money Saver/MerchantTab/Merchant.swift
+++ Money Saver/MerchantTab/Merchant.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Merchant: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var isExpanded: Bool = false
+    var tag: Tag
+}
+
+enum Tag: String, Codable, CaseIterable {
+    case none = "None"
+    case groceries = "Groceries"
+    case homegoods = "Homegoods"
+    
+    var isEmpty: Bool {
+        self == .none ? true : false
+    }
+    
+    var color: Color {
+        switch self {
+        case .none:
+            return Color.gray
+        case .groceries:
+            return Color.green
+        case .homegoods:
+            return Color.red
+        }
+    }
+}
